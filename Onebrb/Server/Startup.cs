1c7f786ee4069128ac0f0f7e6404a3aa5f7ae327
kaldren <drenski666@gmail.com>
2020-06-02 21:45:54 +0300

    Added login as username

--- Onebrb/Server/Startup.cs
+++ Onebrb/Server/Startup.cs
@@ -33,7 +33,7 @@ namespace Onebrb.Server
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
