e872dcd7720e5b675bd812e9582a687e77fa63a1
kaldren <drenski666@gmail.com>
2020-06-03 18:28:55 +0300

    Messages work in progress

--- Onebrb/Server/Controllers/MessageController.cs
+++ Onebrb/Server/Controllers/MessageController.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Onebrb.Server.Data;
+using Onebrb.Server.Models;
 
 namespace Onebrb.Server.Controllers
 {
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
     public class MessageController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public MessageController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<bool> SendMessageAsync(string message)
+        {
+            var messageModel = new Message
+            {
+                AuthorId = "1",
+                Body = message,
+                DateSent = DateTime.UtcNow,
+                RecipientId = "2",
+                Title = "Hi"
+            };
+
+            await _db.Messages.AddAsync(messageModel);
+            await _db.SaveChangesAsync();
+
+            return true;
         }
     }
 }
