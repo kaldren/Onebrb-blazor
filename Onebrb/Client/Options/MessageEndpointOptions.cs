606360a66ab99412913f990946a536aba18bdc92
kaldren <drenski666@gmail.com>
2020-06-05 21:28:48 +0300

    Refactoring messages form

---
+++ Onebrb/Client/Options/MessageEndpointOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onebrb.Client.Options
+{
+    public class MessageEndpointOptions
+    {
+        public const string ApiMessagesEndpoint = "ApiMessagesEndpoint";
+
+        public string CreateMessage { get; set; }
+        public string GetOneMessage { get; set; }
+        public string GetAllMessages { get; set; }
+    }
+}
